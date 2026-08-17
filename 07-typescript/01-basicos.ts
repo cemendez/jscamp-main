0c6b31902449d418bff4628b2ffa03e93e1b5628
Carlos Méndez <carlos.mendez@puebla.gob.mx>
2026-08-17 17:38:20 -0600

    feat: Tipar funciones básicas

---
+++ 07-typescript/01-basicos.ts
@@ -0,0 +1,43 @@
+// FUNCIONES EN TYPESCRIPT
+
+function sumar(a: number, b: number): number {
+    return a + b;
+}
+
+const multipicar = (a: number, b: number): number => {
+    return a * b;
+};
+
+const diviir = (a: number, b: number): number => a / b;
+
+// PARÁMETROS OPCIONALES
+function saludar(nombre: string, apellido?: string): string {
+    if (apellido) {
+        return `Hola, ${nombre} ${apellido}`;
+    }
+
+    return `Hola ${nombre}`;
+}
+
+// PARÁMETROS POR  DEFECTO
+function crearUsuario(nombre: string, rol: string = "admin") {
+    return {
+        nombre,
+        rol,
+    };
+}
+
+// REST PARAMETERS
+function sumarNumeros(...numeros: number[]): number {
+    return numeros.reduce((acc, curr) => acc + curr, 0);
+}
+
+sumarNumeros(1, 2);
+sumarNumeros(1, 2, 3, 4, 5);
+
+// TIPO DE FUNCIÓN (function Types)
+
+type OperacionMatematica = (a: number, b: number) => number;
+
+const division: OperacionMatematica = (a, b) => a / b;
+const resta: OperacionMatematica = (a, b) => a - b;
